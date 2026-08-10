7aa29534cc11477830dcd65711a598ff9ac0cb88
chaithanya-git-it <chaithanya.avinash07@gmail.com>
2026-08-11 01:26:30 +0530

    Updates

--- _next/static/8xRzkvxyaafQWJQvBWDPC/_buildManifest.js
+++ _next/static/8xRzkvxyaafQWJQvBWDPC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/My_Portfolio_website//_next/:path+",
+        "destination": "/My_Portfolio_website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
