0fe0649200d36379c5adeb88e61b2b16feaae15a
chaithanya-git-it <chaithanya.avinash07@gmail.com>
2026-08-11 01:10:25 +0530

    fix: update deploy workflow and add repository basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions) {
+  repo = process.env.GITHUB_REPOSITORY?.replace(/^.*?\//, "") || "";
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: repo ? `/${repo}` : "",
+  assetPrefix: repo ? `/${repo}/` : "",
   images: {
     unoptimized: true,
   },
